6be9e68a9fbce9015805e05dc2a298ca00229cb5
Diogo Ide <diogoide@gmail.com>
2018-04-15 00:44:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,10 @@ class Dog
 
   def name=(dog_name)
     this_dogs_name = dog_name #setter method 
-  end 
+  end
 
   def name
-    this_dogs_name #getter method 
-  end 
-  
-end 
+    this_dogs_name #getter method
+  end
 
-
-  
+end
